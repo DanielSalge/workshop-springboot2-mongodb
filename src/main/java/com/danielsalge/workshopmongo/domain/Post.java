d2f742656038148d6b69a664608e6f07cb3b9d4d
Daniel Solis Salge <danisolsalge82@gmail.com>
2023-01-25 19:19:33 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/danielsalge/workshopmongo/domain/Post.java
@@ -0,0 +1,83 @@
+package com.danielsalge.workshopmongo.domain;
+
+import com.danielsalge.workshopmongo.dto.AuthorDTO;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Document
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private AuthorDTO author;
+
+    public Post() {
+    }
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
