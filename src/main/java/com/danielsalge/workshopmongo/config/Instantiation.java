aa1e786002b5d1001a1948e72c926b6c6777f6f9
Daniel Solis Salge <danisolsalge82@gmail.com>
2023-01-25 19:36:10 -0300

    Referenciando os posts do usuário

--- src/main/java/com/danielsalge/workshopmongo/config/Instantiation.java
+++ src/main/java/com/danielsalge/workshopmongo/config/Instantiation.java
@@ -38,8 +38,11 @@ public class Instantiation implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(alex));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
