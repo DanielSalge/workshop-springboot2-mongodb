8c86143f98fd8b6365ff03747d47d77b43088798
Daniel Solis Salge <danisolsalge82@gmail.com>
2023-01-25 19:49:25 -0300

    Obtendo um post por id

---
+++ src/main/java/com/danielsalge/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.danielsalge.workshopmongo.services;
+
+import com.danielsalge.workshopmongo.domain.Post;
+import com.danielsalge.workshopmongo.domain.User;
+import com.danielsalge.workshopmongo.repository.PostRepository;
+import com.danielsalge.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
