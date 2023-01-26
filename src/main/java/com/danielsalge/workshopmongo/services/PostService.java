11841a99bb20d66c6da954a4e9d05c3f4d2c3b2d
Daniel Solis Salge <danisolsalge82@gmail.com>
2023-01-26 13:12:05 -0300

    Consulta simples com query methods

--- src/main/java/com/danielsalge/workshopmongo/services/PostService.java
+++ src/main/java/com/danielsalge/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.danielsalge.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
